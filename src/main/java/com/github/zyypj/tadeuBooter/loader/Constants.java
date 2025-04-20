2930d9a0975f6053bdc30d0a6f3bdc713bbaf863
Samuel Henrique <150966678+zyypj@users.noreply.github.com>
2025-04-20 20:12:59 -0300

    fix: update packages and name

--- src/main/java/com/github/zyypj/tadeuBooter/loader/Constants.java
+++ src/main/java/com/github/zyypj/tadeuBooter/loader/Constants.java
@@ -6,8 +6,8 @@ import com.google.gson.GsonBuilder;
 
 public class Constants {
     public static final Gson GSON = (new GsonBuilder()).setPrettyPrinting().create();
-    public static String BASE_PACKAGE = "gg.discord.mrkk.core";
-    public static String NAME = "mrk-core";
+    public static String BASE_PACKAGE = "com.github.zyypj.tadeuBooter";
+    public static String NAME = "tadeuBooter";
 
     public static void setBasePackage(String newPackage) {
         BASE_PACKAGE = newPackage;
